e1923483bc631ba10efb258bcbc92e1fe495722b
unknown <akshayathangella14@gmail.com>
2026-06-14 14:33:00 +0530

    Changed CORS

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,12 @@ const app = exp()
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174'
+        'http://localhost:5174',
+        'https://cine-aura-6jojbkavs-akshayathangella14-dels-projects.vercel.app'
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 //body parser middleware
